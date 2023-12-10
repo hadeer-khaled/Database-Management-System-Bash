532b7aed869288e7e94dcdbce8c0fe9a1adb0da1
Hadeer-Khaled <hadeer@192.168.44.172>
2023-12-10 23:25:10 +0200

    add flag

--- DBMS.sh
+++ DBMS.sh
@@ -16,17 +16,43 @@ PS3="choose an action to do: "
 actions=("Create Database" "List Databases" "Connect to Database" "Drop Database" "Exit")
 select action in "${actions[@]}";
 	do    
+	flag=1
 		case $REPLY in
 		    1 | [Cc][Rr][Ee][Aa][Tt][Ee][[:space:]][Dd][Aa][Tt][Aa][Bb][Aa][Ss][Ee] )
 
 			read -p "Enter database name: " DB_Name
 			
-			if [ -d "$currentPath/BD/$DB_Name" ]
-			then
-				echo ">>>>>>>>> Error: This Database already exists"
-			else
-				mkdir "$currentPath/BD/$DB_Name"
-				echo "$DB_Name database is created"   
+			#-------------------------- Check the name length--------------------------#
+			if [ ${#DB_Name} -le 1 -o ${#DB_Name} -ge 10 ]
+			then 
+				echo ">>>>>>>>> Error: Database name length should be from 1 to 64 characters."
+				flag=0
+			fi    	
+							
+			#-------------------------- Check for spaces --------------------------#
+			if [[ $DB_Name =~ [[:space:]] ]]
+			then 
+				echo ">>>>>>>>> Warning: Database name cannot contain spaces, the spaces will be replaced with uderscore(_)"
+				echo ">>>>>>>>> The database name will be \"${DB_Name// /_}\" instead of \"$DB_Name\" "
+				DB_Name=${DB_Name// /_}									
+			fi
+			
+			#-------------------------- Check for sepecial charachters OR if start with number OR if start with (_) --------------------------#
+			if ! [[ $DB_Name =~ ^[a-zA-Z][a-zA-Z0-9_]*$ ]]
+			then 
+				echo ">>>>>>>>> Error: Database name can contain letters, uderscore and numbers only. But not start with number or uderscore"
+				flag=0 
+			fi
+			################################# Create the databse ################################
+			if (( flag == 1 ))
+			then 
+				if [ -d "$currentPath/BD/$DB_Name" ]
+				then
+					echo ">>>>>>>>> Error: This Database already exists"
+				else
+					mkdir "$currentPath/BD/$DB_Name"
+					echo "$DB_Name database is created"   
+				fi
 			fi
 			
 			;;		
@@ -34,8 +60,17 @@ select action in "${actions[@]}";
 			echo "The availabl databases are: "
 			ls  "$currentPath/BD" 
 			;;
+			# connect database	
 		    3 | [Cc][Oo][Nn][Nn][Ee][Cc][Tt][[:space:]][Tt][Oo][[:space:]][Dd][Aa][Tt][Aa][Bb][Aa][Ss][Ee] )
-			echo "Connect to Database"
+		    
+		        read -p "Enter database name: " DB_Name
+			if [ -d "$currentPath/BD/$DB_Name" ]
+			then
+				 ## cd to  $currentPath/DB/$DB_Name
+				 ## let prompt at the new path
+			else
+				echo "This Database don't exist"  
+			fi	
 			;;
 		    4 | [Dd][Rr][Oo][Pp][[:space:]][Dd][Aa][Tt][Aa][Bb][Aa][Ss][Ee] )
 			echo "Drop Database"
@@ -47,8 +82,7 @@ select action in "${actions[@]}";
 			echo "Invalid Action"
 			;;
 		esac				
-	done 
-	
+	done 	
 : '
 ########################### start with number ###########################
 
