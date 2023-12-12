6d6f37fdc077c397465dc732e550798629a39891
Hadeer-khaled <hadeer.awad02@eng-st.cu.edu.eg>
2023-12-12 17:35:29 +0200

    get path

--- DBMS.sh
+++ DBMS.sh
@@ -9,41 +9,30 @@ then
 fi
 
 cd $currentPath/BD
+#echo "Your path now is: `pwd`"
 
+#-----------------------------------------
+#echo "the file path $0"
+#script_directory=$(dirname $0)
+#echo $script_directory
+#---------------------------
 #----------------------------------- create selcet menu ------------------------------------
 PS3="choose an action to do: "
 
 actions=("Create Database" "List Databases" "Connect to Database" "Drop Database" "Exit")
 select action in "${actions[@]}";
 	do    
-	flag=1
+	export flag=1
+	export DB_Name=" "
 		case $REPLY in
+			################################# Create the databse ################################
 		    1 | [Cc][Rr][Ee][Aa][Tt][Ee][[:space:]][Dd][Aa][Tt][Aa][Bb][Aa][Ss][Ee] )
 
 			read -p "Enter database name: " DB_Name
 			
-			#-------------------------- Check the name length--------------------------#
-			if [ ${#DB_Name} -le 1 -o ${#DB_Name} -ge 10 ]
-			then 
-				echo ">>>>>>>>> Error: Database name length should be from 1 to 64 characters."
-				flag=0
-			fi    	
-							
-			#-------------------------- Check for spaces --------------------------#
-			if [[ $DB_Name =~ [[:space:]] ]]
-			then 
-				echo ">>>>>>>>> Warning: Database name cannot contain spaces, the spaces will be replaced with uderscore(_)"
-				echo ">>>>>>>>> The database name will be \"${DB_Name// /_}\" instead of \"$DB_Name\" "
-				DB_Name=${DB_Name// /_}									
-			fi
 			
-			#-------------------------- Check for sepecial charachters OR if start with number OR if start with (_) --------------------------#
-			if ! [[ $DB_Name =~ ^[a-zA-Z][a-zA-Z0-9_]*$ ]]
-			then 
-				echo ">>>>>>>>> Error: Database name can contain letters, uderscore and numbers only. But not start with number or uderscore"
-				flag=0 
-			fi
-			################################# Create the databse ################################
+			source name_checker.sh
+
 			if (( flag == 1 ))
 			then 
 				if [ -d "$currentPath/BD/$DB_Name" ]
@@ -55,42 +44,49 @@ select action in "${actions[@]}";
 				fi
 			fi
 			
-			;;		
+			;;	
+			
+			################################# List available databases ################################	
 		    2 | [Ll][Ii][Ss][Tt][[:space:]][Dd][Aa][Tt][Aa][Bb][Aa][Ss][Ee] )
-			echo "The availabl databases are: "
+			echo "The available databases are: "
 			ls  "$currentPath/BD" 
 			;;
 			
-			# connect database	
+			################################# Connect to a databases ################################	
 		    3 | [Cc][Oo][Nn][Nn][Ee][Cc][Tt][[:space:]][Tt][Oo][[:space:]][Dd][Aa][Tt][Aa][Bb][Aa][Ss][Ee] )
 		    
 		        read -p "Enter database name: " DB_Name
-		        
+		        source name_checker.sh
 		        if (( flag == 1 ))
 		        then
 				if [ -d "$currentPath/BD/$DB_Name" ]
 				then
-					 ## cd to  $currentPath/DB/$DB_Name
-					 ## let prompt at the new path
-					 echo "connect database"
+					 cd "./$DB_Name"
+					 echo "Your path now is: `pwd` " 
+
 				else
 					echo "This Database doesn't exist"  
 				fi
 			fi	
 			;;
+			
+			################################# Drop a databases ################################	
 		    4 | [Dd][Rr][Oo][Pp][[:space:]][Dd][Aa][Tt][Aa][Bb][Aa][Ss][Ee] )
 			read -p "Enter database name: " DB_Name
+			source name_checker.sh
 		        if (( flag == 1 ))
 		        then
 				if [ -d "$currentPath/BD/$DB_Name" ]
 				then
 					
-					 echo "drop database"
+					rm -r "$currentPath/BD/$DB_Name" 
+					echo "$DB_Name is deleted successfully."
 				else
 					echo "This Database doesn't exist"  
 				fi
 		        fi 
 			;;
+			
 		    5 | [Ee][Xx][Ii][Tt] )
 			break
 			;;
--- name_checker.sh
+++ name_checker.sh
@@ -2,25 +2,28 @@
 shopt -s extglob
 
 
-########################### length 1->64 characters ######################
+			#-------------------------- Check the name length--------------------------#
+			if [ ${#DB_Name} -le 1 -o ${#DB_Name} -ge 10 ]
+			then 
+				echo ">>>>>>>>> Error: Database name length should be from 1 to 64 characters."
+				flag=0
+			fi    	
+							
+			#-------------------------- Check for spaces --------------------------#
+			if [[ $DB_Name =~ [[:space:]] ]]
+			then 
+				echo ">>>>>>>>> Warning: Database name cannot contain spaces, the spaces will be replaced with uderscore(_)"
+				echo ">>>>>>>>> The database name will be \"${DB_Name// /_}\" instead of \"$DB_Name\" "
+				DB_Name=${DB_Name// /_}									
+			fi
+			
+			#-------------------------- Check for sepecial charachters OR if start with number OR if start with (_) --------------------------#
+			if ! [[ $DB_Name =~ ^[a-zA-Z][a-zA-Z0-9_]*$ ]]
+			then 
+				echo ">>>>>>>>> Error: Database name can contain letters, uderscore and numbers only. But not start with number or uderscore"
+				flag=0 
+			fi
 
-if [ ${#DB_Name} -le 1 -o ${#DB_Name} -ge 10 ]
-then 
-	echo ">>>>>>>>> Error: Database name length should be from 1 to 64 characters."
-	
-fi    					
-########################### check for space ######################
-if [[ $DB_Name =~ [[:space:]] ]]
-then 
-	echo ">>>>>>>>> Warning: Database name cannot contain spase, the spase will be replaced with uderscore(_)"
-	echo ">>>>>>>>> $DB_Name will be ${DB_Name// /_}"
-	DB_Name=${DB_Name// /_}									
-fi
-########################### check for sepecial charachters , not start with number , not start with _ ###########################
-if ! [[ $DB_Name =~ ^[a-zA-Z][a-zA-Z0-9_]*$ ]]
-then 
-	echo ">>>>>>>>> Error: Database name can contain letters, uderscore and numbers only. But not start with number or uderscore"
-fi
 
 : '
 ########################### length 1->64 characters ######################
