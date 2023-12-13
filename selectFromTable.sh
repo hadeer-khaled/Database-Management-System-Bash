a71e2b14d1346d86333edb733006bd6c677d67a3
nouran <nouran@localhost.localdomain>
2023-12-13 23:13:33 +0200

    add select whole column

--- selectFromTable.sh
+++ selectFromTable.sh
@@ -1,46 +1,53 @@
 #!/usr/bin/bash
 
-#need to combine two files to have hole table
-#from combination select hole table 
-#from combination select hole column
-#from combination select hole row
-
-currentPath=`pwd`
-
-#read -p "If you want to see metadata table with data table, please enter the table name to show: " tableName
-#cat "$tableName.metadata" "$tableName.data"
+currentPath=$(pwd)
 
 while true
 do
     echo "=== HN Select Menu ==="
-    selectMenu=("Select hole table " "Select hole column" "Select hole row" "Exit") 
+    selectMenu=("Select hole table" "Select hole column" "Select hole row" "Exit")
     select answer in "${selectMenu[@]}"
-    do
+     do
         case $answer in
-        "Select hole table ")
-   		 echo "You selected: $answer"
-  		  read -p "PLease enter the name of table you want to select: " tableName
-		 if [[ -f "$currentPath/$tableName.data" && -f "$currentPath/$tableName.metadata" ]]
-  		  then
-  			      cat "$tableName.metadata" "$tableName.data"
-  		elif [[ -f "$currentPath/$tableName.metadata" && ! -f "$currentPath/$tableName.data" ]]
-  		then
-  		echo "this table is empty .. no content just metadata"
-  		cat "$tableName.metadata"
-   		 else
-     			   echo "Sorry, there is no tables with this name"
-   		 fi
-  		  ;;
-
+            "Select hole table")
+                echo "You selected: $answer"
+                read -p "Please enter the name of the table you want to select: " tableName
+                if [[ -f "$currentPath/$tableName.data" && -f "$currentPath/$tableName.metadata" ]]
+                then
+                    cat "$tableName.metadata" "$tableName.data"
+                elif [[ -f "$currentPath/$tableName.metadata" && ! -f "$currentPath/$tableName.data" ]]
+                then
+                    echo "This table is empty .. no content, just metadata"
+                    cat "$tableName.metadata"
+                else
+                    echo "Table '$tableName' not found or incomplete (missing metadata or data file)."
+                fi
+                ;;
+          "Select hole column")
+ 	   echo "You selected: $answer"
+  	  read -p "Please enter the name of the table you want to select a column from: " tableName
+ 	   if [[ -f "$currentPath/$tableName.data" && -f "$currentPath/$tableName.metadata" ]]
+ 	   then
+    	    metadata=$(<"$tableName.metadata")
+   	     read -p "Please enter the name of the column you want to select from $tableName: " columnName
+   	     columnNumber=$(echo "$metadata" | awk -F: -v col="$columnName" '{for (i=1; i<=NF; i++) if ($i == col) print i}')
+  		      if [[ ${#columnNumber} -gt 0 ]]
+   		     then
+       			    awk -F: -v colNum="$columnNumber" '{if (NF >= colNum) print $colNum}' "$currentPath/$tableName.data"
+     		   else
+           		 echo "Column '$columnName' not found in the metadata of '$tableName'."
+        		fi
+    	else
+      	  echo "Table '$tableName' not found or incomplete (missing metadata or data file)."
+   	 fi
+   	 ;;
             "Exit")
                 echo "Exiting program!"
                 exit;;
             *) 
-            echo "Invalid choice. Please enter a valid option.";;
+                echo "Invalid choice. Please enter a valid option.";;
         esac
         break
     done
 done
-
-
 
