ea6cd4b1c456992c13b7471a9e6cca94d5036886
Hadeer-khaled <hadeer.awad02@eng-st.cu.edu.eg>
2023-12-15 02:09:56 +0200

    correct updateTable

--- updateTable.sh
+++ updateTable.sh
@@ -1,34 +1,32 @@
-#!/bin/bash
+#!/usr/bin/bash
 
-# Specify the file containing the data
-tableName="table1.data"
+read -p "Enter the Table Name you want to update it: " tableName
 
-# Specify the column and value to identify the record to update
-whereColumnName="id"
-whereValue="10"
+read -p "Enter the Column Name you want to update it: " desiredColumnName  
+read -p "Enter the New $desiredColumnName value: " newValue 
 
-# Specify the new values for the record
-newValue="hadeer"
+read -p "Enter the Condition Column: " whereColumnName  
+read -p "Enter the Condition Value: " whereValue 
 
-# Temporary file
+
+metaDataFile=$tableName.metadata
+dataFile=$tableName.data
+header=`head -n 1 $metaDataFile`
+
+#field_number=$(echo "$header" | awk -F: 'BEGIN{field_name=$field_name } {for(i=1; i<=NF; i++) if($i == field_name) print i}')
+desiredColumnNumber=$(echo "$header" | awk -F: -v desiredColumnName="$desiredColumnName" '{for(i=1; i<=NF; i++) if($i == desiredColumnName) print i}')
+CondColumnNumber=$(echo "$header" | awk -F: -v whereColumnName="$whereColumnName" '{for(i=1; i<=NF; i++) if($i == whereColumnName) print i}')
+
 temp_file=$(mktemp)
-
-desiredColumnNumber=2
-CondColumnNumber=1
-
-# Check if the record exists and update it
-if grep -q "$whereValue" "$tableName"; then
+if grep -q "$whereValue" "$dataFile"; then
     awk  -v whereValue="$whereValue" \
         -v new_name="$newValue" \
         -v whereColumnName="$whereColumnName" \
         -v CondColumnNumber=$CondColumnNumber\
         -v desiredColumnNumber=$desiredColumnNumber\
-        'BEGIN { FS = OFS =":" } $CondColumnNumber == whereValue { $desiredColumnNumber = new_name } 1' "$tableName" > "$temp_file"
-        cat $tableName
-        cat $temp_file
-        
-    mv "$temp_file" "$tableName"
-            cat $tableName
+        'BEGIN { FS = OFS =":" } $CondColumnNumber == whereValue { $desiredColumnNumber = new_name } 1' "$dataFile" > "$temp_file"
+
+    mv "$temp_file" "$dataFile"
 
     echo "Record with $whereColumnName=$whereValue updated successfully."
 else
@@ -37,3 +35,15 @@ fi
 
 # Clean up temporary file
 rm -f "$temp_file"
+
+
+: '------------------------------------------------------------------------------------
+
+if [[ -n "$ColumnNameNumber" && -n "$whereColumnNameNumber" ]]; then
+    echo "Field number of $ColumnNameNumber is: $ColumnNameNumber"
+    echo "Field number of $whereColumnNameNumber is: $whereColumnNameNumber"
+else
+    echo "Field $field_name not found in the header."
+fi
+----------------------------------------------------------------------------------------'
+
